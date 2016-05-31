9d95f4a26c86bfdf17865aed7cf2c7bc685d4b89
Dewey Hong <dhong@fortinet.com>
2016-05-31 11:20:23 -0700

    .

--- test_cmd/go_cmd_test.go
+++ test_cmd/go_cmd_test.go
@@ -2,299 +2,132 @@ package main
 
 import (
 	"fmt"
-	"golang.org/x/net/html" // HTML 파싱 해키지
+	_ "io/ioutil"
 	"log"
-	"net/http"
+	"math/big"
+	_ "net/http"
 	"runtime"
-	"testing"
+	"strconv"
 	"sync"
+	"testing"
+	"time"
 )
 
-// 팔로잉 결괏값을 저장할 구조체
-type FollowingResult struct {
-	url  string
-	name string
-}
-
-// 별표 저장소 결괏값을 저장할 구조체
-type StarsResult struct {
-	repo string
-}
-
-// 중복 URL을 처리할 구조체
-type FetchedUrl struct {
-	m map[string]error
+var fetched = struct {
+	m map[string]error // 중복 검사를 위한 URL과 에러 값 저장
 	sync.Mutex
+}{m: make(map[string]error)} // 변수를 선언하면서 이름이 없는 구조체를 정의하고
+// 초깃값을 생성하여 대입
+
+type result struct { // 결괏값을 저장할 구조체
+	url  string // 가져온 URL
+	name string // 사용자 이름
 }
 
-//  중복 저장소 이름을 처리할 구조체
-type FetchedRepo struct {
-	m map[string]struct{}
-	sync.Mutex
-}
-
-// Crawler 인터페이스 정의
-type Crawler interface {
-	Crawl()
-}
-
-// 팔로잉 수집 구조체 정의
-type GitHubFollowing struct {
-	fetchedUrl *FetchedUrl          // 중복 URL을 처리할 멤버
-	p          *Pipeline            // 파이프라인에 작업 요청을 보낼 멤버
-	stars      *GitHubStars         // 별표 페이지도 처리해야 하므로 stars 멤버도 포함
-	result     chan FollowingResult // 결괏값을 보낼 멤버
-	url        string               // URL 처리 작업을 요청하기 위한 멤버
-}
-
-// 별표 저장소 수집 구조체 정의
-type GitHubStars struct {
-	fetchedUrl  *FetchedUrl      // 중복 URL을 처리할 멤버
-	fetchedRepo *FetchedRepo     // 중복 저장소 이름을 처리할 멤버
-	p           *Pipeline        // 파이프라인에 작업 요청을 보낼 멤버
-	result      chan StarsResult // 결괏값을 보낼 멤버
-	url         string           // URL 처리 작업을 요청하기 위한 멤버
-}
-
-func fetch(url string) (*html.Node, error) {
-	res, err := http.Get(url)
-	if err != nil {
-		log.Println(err)
-		return nil, err
-	}
-
-	doc, err := html.Parse(res.Body)
-	if err != nil {
-		log.Println(err)
-		return nil, err
-	}
-
+func fetch(url string) (string, error) {
+	//	res, err := http.Get(url) // URL에서 HTML 데이터를 가져옴
+	//	if err != nil {
+	//		log.Println(err)
+	//		return "", err
+	//	}
+	doc := "aaa"
+	//	defer res.Body.Close()
+	//	doc, err := ioutil.ReadAll(res.Body)
 	return doc, nil
 }
 
-func (g *GitHubFollowing) Request(url string) {
-	g.p.request <- &GitHubFollowing{ // 구조체를 생성하여 파이프라인의 request 채널에 보냄
-		fetchedUrl: g.fetchedUrl, // 현재 인스턴스에서 포인터를 가져와서 다시 사용
-		p:          g.p,          // 현재 인스턴스에서 포인터를 가져와서 다시 사용
-		result:     g.result,     // 현재 인스턴스에서 포인터를 가져와서 다시 사용
-		stars:      g.stars,      // 현재 인스턴스에서 포인터를 가져와서 다시 사용
-		url:        url,          // url만 새로운 값
-	}
-}
+func parseFollowing(url string, doc string, urls chan string) <-chan string {
+	content := make(chan string)
 
-// 팔로잉 페이지에서 사용자 이름, 팔로잉 URL, 별표 URL을 구함
-func (g *GitHubFollowing) Parse(doc *html.Node) <-chan string {
-	name := make(chan string)
-
-	go func() {
-		var f func(*html.Node)
-		f = func(n *html.Node) {
-			if n.Type == html.ElementNode && n.Data == "img" {
-				for _, a := range n.Attr {
-					if a.Key == "class" && a.Val == "avatar left" { // class가 avatar left인 요소
-						for _, a := range n.Attr {
-							if a.Key == "alt" {
-								// 사용자 이름을 구한 뒤 채널에 보냄
-								name <- a.Val
-								break
-							}
-						}
-					}
-
-					if a.Key == "class" && a.Val == "avatar" {
-						// 부모 요소의 첫 번째 속성(href)
-						user := n.Parent.Attr[0].Val
-
-						// 팔로잉 URL을 파이프라인에 보냄
-						g.Request("https://github.com" + user + "/following")
-						// 별표 URL을 파이프라인에 보냄
-						g.stars.Request("https://github.com/stars" + user)
-						break
-					}
-				}
-			}
-			for c := n.FirstChild; c != nil; c = c.NextSibling {
-				f(c) // 재귀호출로 자식과 형제를 모두 탐색
-			}
-		}
-		f(doc)
+	go func() { // 교착 상태가 되지 않도록 고루틴으로 실행
+		content <- doc
+		i, _ := strconv.Atoi(url)
+		i++
+		urls <- strconv.Itoa(i)
 	}()
 
-	return name
+	return content // 채널을 리턴
 }
 
-func (g *GitHubFollowing) Crawl() {
-	g.fetchedUrl.Lock()                     // 맵은 뮤텍스로 보호
-	if _, ok := g.fetchedUrl.m[g.url]; ok { // URL 중복 처리 여부를 검사
-		g.fetchedUrl.Unlock()
+func crawl(url string, urls chan string, c chan<- result) {
+	fetched.Lock()                   // 맵은 뮤텍스로 보호
+	if _, ok := fetched.m[url]; ok { // URL 중복 처리 여부를 검사
+		fetched.Unlock()
 		return
 	}
-	g.fetchedUrl.Unlock()
+	fetched.Unlock()
 
-	doc, err := fetch(g.url) // URL에서 파싱된 HTML 데이터를 가져옴
-	if err != nil {          // URL을 가져오지 못했을 때
+	doc, err := fetch(url) // URL에서 파싱된 HTML 데이터를 가져옴
+	if err != nil {        // URL을 가져오지 못했을 때
 		go func(u string) { // 교착 상태가 되지 않도록 고루틴을 생성
-			g.Request(u) // 파이프라인에 URL을 보냄
-		}(g.url)
+			urls <- u // 채널에 URL을 보냄
+		}(url)
 		return
 	}
 
-	g.fetchedUrl.Lock()
-	g.fetchedUrl.m[g.url] = err // 가져온 URL은 맵에 URL과 에러 값 저장
-	g.fetchedUrl.Unlock()
+	fetched.Lock()
+	fetched.m[url] = err // 가져온 URL은 맵에 URL과 에러 값 저장
+	fetched.Unlock()
 
-	name := <-g.Parse(doc)                   // 사용자 이름을 구함
-	g.result <- FollowingResult{g.url, name} // 사용자 이름과 URL을 팔로잉 결과 채널에 보냄
-}
+	doc = <-parseFollowing(url, doc, urls) // 사용자 정보, 팔로잉 URL을 구함
 
-func (g *GitHubStars) Request(url string) {
-	g.p.request <- &GitHubStars{ // 구조체를 생성하여 파이프라인의 request 채널에 보냄
-		fetchedUrl:  g.fetchedUrl,  // 현재 인스턴스에서 포인터를 가져와서 다시 사용
-		fetchedRepo: g.fetchedRepo, // 현재 인스턴스에서 포인터를 가져와서 다시 사용
-		p:           g.p,           // 현재 인스턴스에서 포인터를 가져와서 다시 사용
-		result:      g.result,      // 현재 인스턴스에서 포인터를 가져와서 다시 사용
-		url:         url,           // url만 새로운 값
-	}
-}
-
-// 별표 페이지에서 저장소 이름을 구함
-func (g *GitHubStars) Parse(doc *html.Node) <-chan string {
-	repo := make(chan string)
-
-	go func() {
-		defer close(repo) // 고루틴이 끝나기 직전에  채널을 닫음
-
-		var f func(*html.Node)
-		f = func(n *html.Node) {
-			if n.Type == html.ElementNode && n.Data == "span" { // span 태그
-				for _, a := range n.Attr {
-					if a.Key == "class" && a.Val == "prefix" { // class가 prefix인 요소
-						// 저장소 이름을 구한 뒤 채널에 보냄
-						repo <- n.Parent.Attr[0].Val
-						break
-					}
-				}
-			}
-			for c := n.FirstChild; c != nil; c = c.NextSibling {
-				f(c) // 재귀호출로 자식과 형제를 모두 탐색
-			}
-		}
-		f(doc)
-	}()
-
-	return repo
-}
-
-func (g *GitHubStars) Crawl() {
-	g.fetchedUrl.Lock()                     // 맵은 뮤텍스로 보호
-	if _, ok := g.fetchedUrl.m[g.url]; ok { // URL 중복 처리 여부를 검사
-		g.fetchedUrl.Unlock()
-		return
-	}
-	g.fetchedUrl.Unlock()
-
-	doc, err := fetch(g.url) // URL에서 파싱된 HTML 데이터를 가져옴
-	if err != nil {          // URL을 가져오지 못했을 때
-		go func(u string) { // 교착 상태가 되지 않도록 고루틴을 생성
-			g.Request(u) // 파이프라인에 URL을 보냄
-		}(g.url)
-		return
-	}
-
-	g.fetchedUrl.Lock()
-	g.fetchedUrl.m[g.url] = err // 가져온 URL은 맵에 URL과 에러 값 저장
-	g.fetchedUrl.Unlock()
-
-	repositories := g.Parse(doc)
-	for r := range repositories { // 채널에서 range로 저장소 이름을 계속 꺼냄
-		g.fetchedRepo.Lock()                  // 맵은 뮤텍스로 보호
-		if _, ok := g.fetchedRepo.m[r]; !ok { // 저장소 중복 저장 여부 검사
-			g.result <- StarsResult{r}      // 저장소 이름을 별표 저장소 결과 채널에 보냄
-			g.fetchedRepo.m[r] = struct{}{} // 저장한 저장소 이름은 맵에 저장
-		}
-		g.fetchedRepo.Unlock()
-	}
-}
-
-// Crawler 인터페이스를 처리할 파이프라인 구조체 정의
-type Pipeline struct {
-	request chan Crawler // Crawler 타입으로 채널 선언
-	done    chan struct{}
-	wg      *sync.WaitGroup
-}
-
-func NewPipeline() *Pipeline {
-	return &Pipeline{ // 새 파이프라인 생성
-		request: make(chan Crawler),
-		done:    make(chan struct{}),
-		wg:      new(sync.WaitGroup),
-	}
+	log.Printf("---- %s %s", url, doc)
+	c <- result{url, doc} // 가져온 URL과 사용자 이름을 구조체 인스턴스로
+	// 생성하여 채널 c에 보냄
 }
 
 // 실제 작업을 처리하는 worker 함수
-func (p *Pipeline) Worker() {
-	for r := range p.request { // request 채널에서 Crawler 인터페이스를 가져옴
-		select {
-		case <-p.done: // 채널이 닫히면 worker 함수를 빠져나옴
-			return
-		default:
-			r.Crawl() // Crawler 인터페이스의 Crawl 함수 실행
-		}
+func worker(done <-chan struct{}, urls chan string, c chan<- result) {
+	for url := range urls { // urls 채널에서 URL을 가져옴
+		//		select {
+		//		case <-done: // 채널이 닫히면 worker 함수를 빠져나옴
+		//			return
+		//		default:
+		crawl(url, urls, c) // URL 처리
+		//		}
 	}
-}
-
-func (p *Pipeline) Run() {
-	const numWorkers = 10
-	p.wg.Add(numWorkers)
-	for i := 0; i < numWorkers; i++ { // 작업을 처리할 고루틴 10개 생성
-		go func() {
-			p.Worker()
-			p.wg.Done()
-		}()
-	}
-
-	go func() {
-		p.wg.Wait() // 작업 고루틴이 끝날 때까지 대기
-	}()
 }
 
 func TestMap(t *testing.T) {
+	start := time.Now()
+	r := new(big.Int)
+	fmt.Println(r.Binomial(1000, 10))
+
 	numCPUs := runtime.NumCPU()
 	runtime.GOMAXPROCS(numCPUs) // 모든 CPU를 사용하도록 설정
 
-	p := NewPipeline() // 파이프라인 인스턴스 생성
-	p.Run()            // worker 함수를 고루틴으로 생성
+	urls := make(chan string)   // 작업을 요청할 채널
+	done := make(chan struct{}) // 작업 고루틴에 정지 신호를 보낼 채널
+	c := make(chan result)      // 결괏값을 저장할 채널
 
-	stars := &GitHubStars{ // 별표 구조체 인스턴스 생성
-		fetchedUrl:  &FetchedUrl{m: make(map[string]error)},     // 중복 URL 처리 맵
-		fetchedRepo: &FetchedRepo{m: make(map[string]struct{})}, // 중복 저장소
-		// 이름 처리 맵
-		p:      p,                      // 파이프라인 인스턴스
-		result: make(chan StarsResult), // 별표 저장소
-		// 결괏값 채널
+	var wg sync.WaitGroup
+	const numWorkers = 5
+	wg.Add(numWorkers)
+	for i := 0; i < numWorkers; i++ { // 작업을 처리할 고루틴을 10개 생성
+		go func() {
+			worker(done, urls, c)
+			//			wg.Done()
+		}()
 	}
-	following := &GitHubFollowing{ // 팔로잉 구조체 인스턴스 생성
-		fetchedUrl: &FetchedUrl{m: make(map[string]error)}, // 중복 URL 처리 맵
-		p:          p,                                      // 파이프라인 인스턴스
-		result:     make(chan FollowingResult),             // 팔로잉 결괏값 채널
-		stars:      stars,                                  // GitHubStars 구조체 인스턴스
-		url:        "https://github.com/pyrasis/following", // 최초 시작 URL
-	}
-	p.request <- following // 파이프라인에 구조체 인스턴스를 보내서 HTML 처리 작업 시작
+
+	//	go func() {
+	//		wg.Wait() // 고루틴이 끝날 때까지 대기
+	//		close(c)  // 대기가 끝나면 결괏값 채널을 닫음
+	//	}()
+
+	urls <- "1" // 최초 URL을 보냄
 
 	count := 0
-LOOP:
-	for { // 무한 루프 사용
-		select { // 채널에 결괏값이 들어왔을 때마다 값을 출력
-		case f := <-following.result:
-			fmt.Println(f.name)
-		case s := <-stars.result:
-			fmt.Println(s.repo)
-			if count == 1000 { // 저장소를 1,000개만 출력
-				close(p.done) // done 채널을 닫음
-				break LOOP    // 무한 루프를 빠져나옴
-			}
-			count++
+	for r := range c { // 결과 채널에 값이 들어올 때까지 대기한 뒤 값을 가져옴
+		fmt.Println(r.name)
+
+		count++
+		if count > 2 { // 100명만 출력한 뒤
+			close(done) // done을 닫아서 worker 고루틴을 종료
+			break
 		}
 	}
+
+	elapsed := time.Since(start)
+	log.Printf("It took %s", elapsed)
 }
